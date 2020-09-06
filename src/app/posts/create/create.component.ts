2a9a2e1a9bdc80a8d822bc321da8df42afc474a1
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-09-06 14:27:45 +0800

    任務06: Reactive Forms 繫結表單資料

--- src/app/posts/create/create.component.ts
+++ src/app/posts/create/create.component.ts
@@ -1,14 +1,46 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormArray } from '@angular/forms';
 
 @Component({
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.css']
 })
 export class CreateComponent implements OnInit {
+  post = this.formbuilder.group({
+    title: this.formbuilder.control(''),
+    description: this.formbuilder.control(''),
+    body: this.formbuilder.control(''),
+    tags: this.formbuilder.array([
+      this.formbuilder.control('Angular'),
+      this.formbuilder.control('HTML'),
+      this.formbuilder.control('CSS')
+    ]),
+  });
 
-  constructor() { }
+  constructor(private formbuilder: FormBuilder) { }
 
   ngOnInit(): void {
   }
 
+  get tags(): FormArray {
+    return this.post.get('tags') as FormArray;
+  }
+
+  addTag(tag: string): void {
+    if (tag.trim().length <= 0) {
+      return;
+    }
+
+    this.tags.push(
+      this.formbuilder.control(tag)
+    );
+  }
+
+  removeTag(index: number): void {
+    this.tags.removeAt(index);
+  }
+
+  createPost(): void {
+    console.log(this.post.value);
+  }
 }
