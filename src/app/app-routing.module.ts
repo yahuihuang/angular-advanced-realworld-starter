5b3336d155666e9ef834576c5d8fc951a482bf9e
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-09-06 09:49:37 +0800

    任務01

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+import { LayoutComponent } from './layout/layout.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LayoutComponent,
+    loadChildren: () => import('./posts/posts.module').then(m => m.PostsModule)
+  },
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
