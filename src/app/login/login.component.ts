ab4ee7c2b6d5af69e22c93d7e13fdcd6dfea2b3f
huang.yahui.grace <huang.yahui.grace@gmail.com>
2020-09-06 11:58:22 +0800

    任務04: 在程式中動態切換路由

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,10 @@
+import { UserLoginInfo } from './../interfaces/login-info';
+import { LoginService } from './../login.service';
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
+import { catchError } from 'rxjs/operators';
+import { throwError } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +13,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  user: UserLoginInfo = {
+    email: '',
+    password: ''
+  };
+
+  constructor(private router: Router, private loginService: LoginService) {
+
+  }
 
   ngOnInit(): void {
   }
 
+  login(): void {
+    console.log('in login function');
+    this.loginService.login(this.user).pipe(
+      catchError((error: HttpErrorResponse) => {
+        alert(error.message);
+        return throwError(error);
+      })
+    ).subscribe(result => {
+      localStorage.setItem('token', result.user.token);
+      this.router.navigateByUrl('/');
+    });
+  }
 }
